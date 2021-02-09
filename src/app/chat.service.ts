f8570f3d9437e2d6f145d6085f6bae32fa5d1ad7
Santhosh <santhoshprogrammer94@gmail.com>
2021-02-09 18:22:54 +0530

    trying auth

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -11,10 +11,18 @@ export class ChatService {
   private socket: any;
   private readonly _receiveData = new BehaviorSubject<any>([]);
   readonly receiveData$ = this._receiveData.asObservable();
-  
+  socketOptions = {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNjEyODY3OTM0LCJleHAiOjE2MTI5NTQzMzR9.AzBMD9AdfhPnl3_rzJi1l5SQjCLWcOW1zCQs0N-QgL4', //'Bearer h93t4293t49jt34j9rferek...'
+        }
+      }
+    }
+ };
   constructor() {
     // Connect Socket with server URL
-    this.socket = io(environment.socketUrl);
+    this.socket = io(environment.socketUrl,{ query:{token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNjEyODY3OTM0LCJleHAiOjE2MTI5NTQzMzR9.AzBMD9AdfhPnl3_rzJi1l5SQjCLWcOW1zCQs0N-QgL4'}});
   }
 
   receiveMessage(){
